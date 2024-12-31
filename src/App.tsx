f6ab429718333d3e8c7255b9f65d769c2dfb5f0c
MaximoXv <maximoibarra164@gmail.com>
2024-12-30 22:12:32 -0300

    cards and clx feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useEffect, useState } from "react"
-import Cards from "./components/cards/cards"
+import Cards, { Character } from "./components/cards/cards"
 import Filters from "./components/filters/filters"
 
+interface DataApi {
+  info: Record<string , any>;
+  results: Character[];
+}
+
 function App() {
-  const [pageNumber,setPageNumber] = useState(1);
+  const [pageNumber] = useState<number>(1);
+  const [fetchedData, setFetchedData] = useState<DataApi | undefined>(undefined);
+  
+  let info, results;
+  if(fetchedData){
+    ({info, results} = fetchedData);
+  }
+
+  console.log(results);
+  console.log(info);
+  
+
   const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
 
   useEffect(()=>{
     (async function(){
       const data = await fetch(api);
-      const datajson = await data.json();
-      console.log(datajson);
+      const dataJson = await data.json();
+      setFetchedData(dataJson)
     })();
   },[api]);
 
 
   return (
     <div>
-      <h1 className="text-center font-extrabold my-3 text-2xl">Rick & Morty</h1>
+      <h1 className="text-center font-semibold my-3 text-2xl">Rick & Morty <span className="font-extrabold">WiKi</span></h1>
       <div className="grid gap-4 grid-cols-12">
         <div className="col-span-1"></div>
         <div className="col-span-3 bg-red-300">
           <Filters/>
         </div>
         <div className="grid gap-2 grid-cols-12 col-span-7">
-          <Cards/>
-          <Cards/>
-          <Cards/>
+          <Cards results={results}/>
         </div>
         <div className="col-span-1"></div>
       </div>
--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,7 +1,55 @@
-const Cards = () => {
-  return (
-    <div className="col-span-4 bg-slate-500">Cards</div>
-  )
+import { clx } from "../../utils/clx";
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+  location: Record<string, string>;
 }
 
-export default Cards
+interface CardsProps {
+  results: Character[] | undefined;
+}
+
+const Cards = ({ results }: CardsProps) => {
+  let display;
+  if (results) {
+    display = results.map((character) => {
+      const { id, name, status, image, location } = character;
+      return (
+        <div key={id} className="col-span-4 relative border-2 border-blue-500 rounded-lg overflow-hidden">
+          <div>
+            <img src={image} alt="" className="w-full" />
+            <div className="p-3">
+              <div className="text-xl font-bold mb-4">{name}</div>
+              <div>
+                <div className="text-sm font-light">Last location</div>
+                <div className="text-md">{location.name}</div>
+              </div>
+            </div>
+          </div>
+          <div
+            className={clx(
+              "absolute top-1 right-1 py-1 px-2 rounded-md text-sm font-bold",
+              status == "Alive" ? "bg-green-500" : "",
+              status == "unknown" ? "bg-gray-500" : "",
+              status == "Dead" ? "bg-red-500" : ""
+            )}
+          >
+            {status}
+          </div>
+        </div>
+      );
+    });
+  } else {
+    display = "No characters found :/";
+  }
+
+  return <>{display}</>;
+};
+
+export default Cards;
